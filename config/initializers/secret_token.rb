d5e9191b6d6f7418c645301aa508c6037c6e0f91
niibori <zzz222.duck@gmail.com>
2013-04-02 20:49:02 +0900

    create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mogu::Application.config.secret_key_base = '70dfdd40d4ce93ae3d07c3c1eb36edd61dfb2b8897d0752c33e56539a94a5c544b0386b7775255a20f01b9797bd2e5a59e6e87629988be34def416b1f611ae23'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mogu::Application.config.session_store :encrypted_cookie_store, key: '_mogu_session'
